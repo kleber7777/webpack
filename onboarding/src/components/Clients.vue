108756281c1afd119bec3f6e06ed6d5fe931d4f0
Kleber <kleber7777@gmail.com>
2017-05-29 15:18:11 -0300

    Adição de SASS

--- onboarding/src/components/Clients.vue
+++ onboarding/src/components/Clients.vue
@@ -8,40 +8,42 @@
 </template>
 
 <script>
-import Moment from 'moment';
-import Inputmask from 'inputmask';
+  import Moment from 'moment';
+  import Inputmask from 'inputmask';
 
-export default {
-  name: 'clients',
-  data() {
-    return {
-      msg: Moment().format(),
-      available: true,
-      services: [
-        {
-          name: 'servico 1',
-        },
-        {
-          name: 'servico 2',
-        },
-      ],
-    };
-  },
-  methods: {
-    checkout() {
-      this.msg = 'trocou!!!';
-      this.services.push({ name: 'servico 3' });
-
-      const selector = document.getElementById('selector');
-
-      const im = new Inputmask('99-9999999');
-      im.mask(selector);
+  export default {
+    name: 'clients',
+    data() {
+      return {
+        msg: Moment().format(),
+        available: true,
+        services: [
+          {
+            name: 'servico 1',
+          },
+          {
+            name: 'servico 2',
+          },
+        ],
+      };
     },
-  },
-};
+    methods: {
+      checkout() {
+        this.msg = 'trocou!!!';
+        this.services.push({ name: 'servico 3' });
+
+        const selector = document.getElementById('selector');
+
+        const im = new Inputmask('99-9999999');
+        im.mask(selector);
+      },
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="scss" scoped>
+  h1, h2 {
+    font-size: 45px;
+  }
 </style>
