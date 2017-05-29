624136ad9d944bae79b03b4421740cf1e127ba23
Kleber <kleber7777@gmail.com>
2017-05-29 11:42:43 -0300

    Implementação teste com o Firebase

--- onboarding/src/App.vue
+++ onboarding/src/App.vue
@@ -1,13 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
+    <p>Lista:</p>
+    <ul>
+      <li v-for="book in books">{{book.author}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBDqgyYadQ4NOXo_gJssggq6ZTRAUn3Uuw',
+  authDomain: 'onboarding-f1aaf.firebaseapp.com',
+  databaseURL: 'https://onboarding-f1aaf.firebaseio.com',
+  projectId: 'onboarding-f1aaf',
+  storageBucket: 'onboarding-f1aaf.appspot.com',
+  messagingSenderId: '894435447125',
+};
+
+const app = Firebase.initializeApp(config);
+const db = app.database();
+
+const booksRef = db.ref('books');
+
 export default {
   name: 'app',
+  firebase: {
+    books: booksRef,
+  },
 };
 </script>
 
