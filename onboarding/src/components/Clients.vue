2701d5e7aad09f3e287eec41b6132a628030578f
Kleber <kleber7777@gmail.com>
2017-05-29 15:06:09 -0300

    Teste com Pug

--- onboarding/src/components/Clients.vue
+++ onboarding/src/components/Clients.vue
@@ -1,12 +1,42 @@
-<template>
-  <p>clientes</p>
+<template lang="pug">
+  div
+    h1 Clientes: {{ msg }}
+    input(id="selector" type="text" v-model="msg")
+    ul
+      li(v-for="serv in services") {{ serv.name }}
+    button(v-if="available" type="button" @click="checkout()") Click
 </template>
 
 <script>
+import Moment from 'moment';
+import Inputmask from 'inputmask';
+
 export default {
   name: 'clients',
   data() {
-    return {};
+    return {
+      msg: Moment().format(),
+      available: true,
+      services: [
+        {
+          name: 'servico 1',
+        },
+        {
+          name: 'servico 2',
+        },
+      ],
+    };
+  },
+  methods: {
+    checkout() {
+      this.msg = 'trocou!!!';
+      this.services.push({ name: 'servico 3' });
+
+      const selector = document.getElementById('selector');
+
+      const im = new Inputmask('99-9999999');
+      im.mask(selector);
+    },
   },
 };
 </script>
